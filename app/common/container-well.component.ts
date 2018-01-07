82905c222e30c62231a6815f3b3ac1bd48c4737c
Srikanth Kondaparthy <srk289@gmail.com>
2018-01-07 13:27:29 +1100

    Demos content projection

--- app/common/container-well.component.ts
+++ app/common/container-well.component.ts
@@ -1,17 +1,20 @@
-import { Component, Input } from "@angular/core"
+import { Component } from "@angular/core"
 
 @Component({
     selector: 'container-well',
     template: `
         <div class="col-md-12">
-            <div class="well">
-                <h4>{{ title }}</h4>
-                <ng-content></ng-content>
+            <div class="well" (click)="toggleVisible()">
+                <h4><ng-content select="[well-title]"></ng-content></h4>
+                <ng-content *ngIf="visible" select="[well-body]"></ng-content>
             </div>
         </div>
     `
 })
 
 export class ContainerWellComponent {
-    @Input() title:string
+    visible:boolean = true
+    toggleVisible() {
+        this.visible = !this.visible
+    }
 }
