bde432987b0e6d1dc1b4e82f3d872e596deefa84
Srikanth Kondaparthy <srikanth.kondaparthy@ge.com>
2018-01-10 17:44:47 +1100

    Updates

--- app/events/shared/index.ts
+++ app/events/shared/index.ts
@@ -1,6 +1,5 @@
 export * from './events-model'
 export * from './events-service'
-export * from './validator-service'
 export * from './duration.pipe'
 export * from './voting.component'
 export * from './voting.service'
